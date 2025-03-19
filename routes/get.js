e7c4623ce4d91096a8a462e8d892667fbcc20b6f
El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>
2025-03-19 03:33:49 +0000

    get.js
    Signed-off-by: El HATIMI Zakaria <73597675+Zack4DEV@users.noreply.github.com>

--- routes/get.js
+++ routes/get.js
@@ -6,7 +6,6 @@ import is from '@sindresorhus/is'
 import getGravatarUrl from 'gravatar-url'
 import createHtmlElement from 'create-html-element'
 import {renderFile} from 'ejs'
-
 import loadUser from '../lib/load-user.js'
 import loadOptions from '../lib/load-options.js'
 
@@ -16,22 +15,21 @@ function getCopyrightName(user, isPlainText) {
   if (is.string(user)) {
     return user
   }
-
   const copyright = user.name || user.copyright
-
   return isPlainText ? copyright : htmlEscape(copyright)
 }
 
 function getCopyrightHtml(user, isPlainText) {
   const name = getCopyrightName(user, isPlainText)
-  let html = user.url ? createHtmlElement({
-    name: 'a',
-    attributes: {
-      href: user.url,
-    },
-    text: name,
-  }) : name
-
+  let html = user.url
+    ? createHtmlElement({
+        name: 'a',
+        attributes: {
+          href: user.url,
+        },
+        text: name,
+      })
+    : name
   if (user.email) {
     html += ` &lt;${createHtmlElement({
       name: 'a',
@@ -41,7 +39,6 @@ function getCopyrightHtml(user, isPlainText) {
       text: user.email,
     })}&gt;`
   }
-
   return html
 }
 
@@ -50,14 +47,13 @@ function getGravatarEmail(user) {
     // Supports regular format
     return user.email.trim().toLowerCase()
   }
-
   if (is.object(user.copyright[0]) && user.gravatar) {
     // Supports multi-user format
     return user.copyright[0].email.trim().toLowerCase()
   }
 }
 
-const removeFalsy = array => array.filter(Boolean)
+const removeFalsy = (array) => array.filter(Boolean)
 
 export default async function getRoute(request, response) {
   let user
@@ -66,7 +62,7 @@ export default async function getRoute(request, response) {
   } catch ({message}) {
     response
       .status(500)
-      .send(`An internal error occurred - open an issue on https://github.com/remy/mit-license with the following information: ${message}`)
+      .send(`An internal error occurred - open an issue on https://github.com/Zeh4DEV/mit-license with the following information: ${message}`)
     return
   }
 
@@ -77,24 +73,24 @@ export default async function getRoute(request, response) {
   const license = (options.license || user.license).toUpperCase()
   const format = options.format || user.format
   const isPlainText = format !== 'html'
-
   let name
 
   // No error and valid
   if (user.copyright) {
     if (is.string(user.copyright)) {
       name = getCopyrightHtml(user, isPlainText)
-    } else if (is.array(user.copyright) && user.copyright.every(value => is.string(value))) {
+    } else if (is.array(user.copyright) && user.copyright.every((value) => is.string(value))) {
       // Supports: ['Remy Sharp', 'Richie Bendall']
-      name = user.copyright.map(value => (isPlainText ? value : htmlEscape(value))).join(', ')
+      name = user.copyright
+        .map((value) => (isPlainText ? value : htmlEscape(value)))
+        .join(', ')
     } else {
-      name = user.copyright.map(value => getCopyrightHtml(value)).join(', ')
+      name = user.copyright.map((value) => getCopyrightHtml(value)).join(', ')
     }
   }
 
   let gravatar
   const gravatarEmail = getGravatarEmail(user)
-
   if (gravatarEmail) {
     gravatar = createHtmlElement({
       name: 'img',
@@ -107,15 +103,17 @@ export default async function getRoute(request, response) {
   }
 
   try {
-    const content = await renderFile(path.join(directoryName, '..', 'licenses', `${license}.ejs`), {
-      info: `${year} ${name}`,
-      theme: user.theme || 'default',
-      gravatar,
-    })
+    const content = await renderFile(
+      path.join(directoryName, '..', 'licenses', `${license}.ejs`),
+      {
+        info: `${year} ${name}`,
+        theme: user.theme || 'default',
+        gravatar,
+      }
+    )
 
     if (format === 'txt') {
       const {articleContent} = content.match(/<article>(?<articleContent>.*)<\/article>/ms).groups
-
       response
         .set('Content-Type', 'text/plain; charset=UTF-8')
         .send(htmlUnescape(stripHtml(articleContent)).trim())
@@ -127,8 +125,8 @@ export default async function getRoute(request, response) {
       return
     }
 
-    response.json({...user, ...options})
+    response.json({ ...user, ...options })
   } catch (error) {
     response.status(500).send(error)
-  }
+  }
 }
